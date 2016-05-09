d8c21b70fd6d913f7274782d1a017b9766fdcdd4
abu <tareqbuet@gmail.com>
2016-05-08 23:13:25 -0500

    promo slider images fixed

--- home/static/home/assets/js/plugins/promo.js
+++ home/static/home/assets/js/plugins/promo.js
@@ -1,4 +1,5 @@
-$(function() {
-  $(".fullscreen-static-image").backstretch(["assets/img-temp/promo/img1.jpg", "assets/img-temp/promo/img2.jpg"],
-  {duration: 5000, fade:1500});
+$(function () {
+    $(".fullscreen-static-image").backstretch(["/static/home/assets/img-temp/promo/laptop_small.jpg",
+                                                "/static/home/assets/img-temp/promo/blog_small.jpg"],
+        {duration: 5000, fade: 1500});
 });
